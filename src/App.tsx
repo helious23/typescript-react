8c4bd488c3fb2651d91f13c93e470e38ec1d3eab
Max16 <max16@naver.com>
2021-05-10 14:48:44 +0900

    #8.5 React Porps and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import Number from './Number';
 
 interface IState {
   counter:number
 }
 
-class App extends Component<{}, IState > {
+// class component 에서는 Component 다음에 <{props}, {state} > 의 타입을 적어줘야 됨
+// 필요시 interface 로 생성하여 입력
+class App extends Component <{}, IState > {
   state = {
     counter: 0
   }
   render(){
     const{counter} = this.state
     return <div>
-      {counter}
+      <Number count={counter}/>
       <button onClick={this.add}>add</button>
     </div>;
   }
