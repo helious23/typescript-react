e0a05a88b711bac8404401ba91e37e101455e85c
Max16 <max16@naver.com>
2021-06-26 12:03:27 +0900

    #8.6 React Events and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,47 @@
-import React, { Component } from 'react';
-import { Form, Input } from './Input';
-import Number from './Number';
+import React, { Component } from "react";
+import Number from "./Number";
+import { Form, Input } from "./Input";
 
 interface IState {
-  counter:number;
-  name: string,
+  counter: number;
+  name: string;
 }
 
-// class component 에서는 Component 다음에 <{props}, {state} > 의 타입을 적어줘야 됨
-// 필요시 interface 로 생성하여 입력
-class App extends Component <{}, IState > {
+class App extends Component<{}, IState> {
   state = {
     counter: 0,
-    name:""
+    name: "",
+  };
+  render() {
+    const { counter, name } = this.state;
+    return (
+      <div>
+        <Form onFormSubmit={this.onFormSubmit}>
+          <Input value={name} onChange={this.onChange}></Input>
+        </Form>
+        <Number count={counter} />
+        <button onClick={this.add}>add</button>
+      </div>
+    );
   }
-  render(){
-    const{counter, name} = this.state
-    return <div>
-      <Form onFormSubmit={this.onFormSubmit}>
-        <Input value={name} onChange={this.onChange}></Input>
-      </Form>
-      <h3>{name}</h3>
-      <Number count={counter}/>
-      <button onClick={this.add}>add</button>
-    </div>;
-  }
-  onChange = (event:React.SyntheticEvent<HTMLInputElement>) => {
-    const {currentTarget:{value:name}} = event;
+
+  onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
     this.setState({
-      name
+      name:event.currentTarget.value
     })
+    console.log(this.state.name)
+  };
+
+  onFormSubmit= (event:React.FormEvent) => {
+    event.preventDefault()
   }
 
-  onFormSubmit = (event:React.FormEvent) => {
-    event.preventDefault();
-  }
-
-  add = () => {
-    this.setState(prev => {
+  add = (): void => {
+    this.setState((prev) => {
       return {
-        counter: prev.counter + 1
-      }
-    })
-  }
-} 
-
+        counter: prev.counter + 1,
+      };
+    });
+  };
+}
 export default App;
--- src/Number.tsx
+++ src/Number.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import styled from "styled-components"
 
-
-const Container =styled.span<{isBlue:boolean}>`
-    color:${props => props.isBlue? props.theme.blueColor:"black"}
-`;
+const Container = styled.span``
 
 interface IProps {
     count: number
 }
-// 함수형 component 에서는 함수명:React.FunctionComponent<interface object(ex:IProps)> or <{count:number}>
-const Number:React.FunctionComponent<IProps> = ({count}) => (
-<Container isBlue={count >10}>{count}</Container>
-)
 
-export default Number;
+const Number: React.FunctionComponent<IProps> = ({ count }) => <Container>{count}</Container>
+
+export default Number
